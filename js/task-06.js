a3dc92db54d428bed78f21e4d00c19d574a099be
Juliusz Kamiński <107152863+jkamin61@users.noreply.github.com>
2022-12-04 23:05:44 +0100

    Add files via upload

--- js/task-06.js
+++ js/task-06.js
@@ -4,18 +4,16 @@ const dataLength = numbersInput.getAttribute("data-length");
 numbersInput.setAttribute("maxlength", dataLength);
 numbersInput.addEventListener("input", () => {
     console.log(numbersInput.value);
-    total.push(numbersInput.value);
+    total = numbersInput.value;
     if (total.length === parseInt(dataLength)) {
         numbersInput.style.borderColor = "#4caf50";
     } else {
         numbersInput.style.borderColor = "#f44336";
     }
+
 });
 
-numbersInput.addEventListener("blur", () => {
-   total = [];
-   numbersInput.value = "";
-});
+
 
 
 
